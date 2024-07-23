537ebbe22a45f9214c30e0b8f04981b49d73f322
Dries <dries.deschoolmeester@student.howest.be>
2024-07-23 15:00:55 +0200

    added API call

--- app/src/main/java/com/example/mobsecproject/LyricsScreen.kt
+++ app/src/main/java/com/example/mobsecproject/LyricsScreen.kt
@@ -5,18 +5,33 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.Dispatchers
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONObject
 
 @Composable
-fun LyricsScreen(modifier: Modifier = Modifier, navController: NavController){
+fun LyricsScreen(modifier: Modifier = Modifier, navController: NavController, artist:String = "Rick Astley", title: String = "Never Gonna Give You Up"){
+    var lyrics by remember { mutableStateOf<String>(R.string.rickroll.toString())}
+    val scope = rememberCoroutineScope()
+
+    LaunchedEffect(Unit) {
+        scope.launch {
+            lyrics = getLyrics(artist,title)
+        }
+    }
+
     Column(modifier = modifier) {
-        Text(text="Rick Astley - Never Gonna Give You Up", fontSize = 40.sp, lineHeight = 45.sp)
+        Text(text="$artist - $title", fontSize = 40.sp, lineHeight = 45.sp)
         LazyColumn {
-            item { Text(stringResource(R.string.rickroll)) }
+            item { Text(lyrics) }
             //https://api.lyrics.ovh/v1/Rick%20Astley/Never%20Gonna%20Give%20You%20Up
         }
         Spacer(modifier = Modifier.weight(1f))
@@ -24,4 +39,25 @@ fun LyricsScreen(modifier: Modifier = Modifier, navController: NavController){
             Text("Back")
         }
     }
+}
+
+suspend fun getLyrics(artist: String, title: String): String {
+    val client = OkHttpClient()
+    val url = HttpUrl.Builder()
+        .scheme("https")
+        .host("api.lyrics.ovh")
+        .addEncodedPathSegments("v1/$artist/$title")
+        .build()
+    val request = Request.Builder()
+        .url(url)
+        .build()
+
+    return withContext(Dispatchers.IO){
+        client.newCall(request).execute().use { response ->
+            val resposebody = response.body?.string()
+            val jsonObject = JSONObject(resposebody ?: "")
+            jsonObject.getString("lyrics")
+        }
+    }
+
 }
